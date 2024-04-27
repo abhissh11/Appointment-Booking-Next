7c38b8696ec3e6dbe1a0a6c248380ecd0ae27b57
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-04-27 17:15:59 +0530

    API Integ && Category Lists

--- app/_components/CategorySearch.js
+++ app/_components/CategorySearch.js
@@ -1,15 +1,30 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import GlobalApi from "../_utils/GlobalApi";
+import Image from "next/image";
 
 export default function CategorySearch() {
+  const [categoryList, setCategoryList] = useState();
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
+  const getCategoryList = () => {
+    GlobalApi.getCategory().then((res) => {
+      console.log(res.data.data);
+      setCategoryList(res.data.data);
+    });
+  };
+
   return (
-    <div className="m-10 flex items-center flex-col gap-2">
+    <div className="m-2 flex items-center flex-col gap-2">
       <h2 className="font-bold text-4xl tracking-wide ">
         Search <span className="text-primary">Doctors</span>
       </h2>
-      <h3 className="text-gray-500 text-lg">
+      <h3 className="text-gray-500 text-lg px-2">
         Search your doctors and book appointments
       </h3>
       <div className="flex w-full max-w-sm items-center space-x-2 mt-5">
@@ -17,6 +32,29 @@ export default function CategorySearch() {
         <Button type="submit">
           <Search className="h-4 w-4 mr-2" /> Search
         </Button>
+      </div>
+      <div className="grid grid-cols-3 mt-5 md:grid-cols-4 lg:grid-cols-6">
+        {categoryList &&
+          categoryList.map(
+            (cat, index) =>
+              index < 6 && (
+                <div
+                  className="flex flex-col items-center p-5 bg-blue-100 m-2 rounded-lg text-center gap-2 
+                   hover:scale-110 transition-all ease-in-out cursor-pointer"
+                  key={index}
+                >
+                  <Image
+                    src={cat.attributes?.Icon.data.attributes?.url}
+                    alt="icon"
+                    width={40}
+                    height={40}
+                  />
+                  <label className="text-blue-600 text-sm px-2">
+                    {cat?.attributes?.Name}
+                  </label>
+                </div>
+              )
+          )}
       </div>
     </div>
   );
--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -2,16 +2,28 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 
+const container = (delay) => ({
+  hideen: { x: -100, opacity: 0 },
+  hideen: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } },
+});
+
 export default function Hero() {
   return (
     <div>
       <section>
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-            <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-              <img
-                alt=""
-                src="https://media.istockphoto.com/id/1555515663/photo/happy-portrait-group-and-doctors-for-healthcare-service-leadership-and-teamwork-in-hospital.webp?b=1&s=170667a&w=0&k=20&c=vQBhyaQn47KIuZ61OtLG8LTdC9NfIbzonAaSFRjFRN8="
+            <div
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full"
+            >
+              <Image
+                alt="doctors"
+                src="/doctor.jpg"
+                width={800}
+                height={800}
                 className="absolute inset-0 h-full w-full object-cover rounded-3xl "
               />
             </div>
